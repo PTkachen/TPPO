ecef6c817e50cac41354bedb6deba5dcedf58034
PTkachen <83370779+PTkachen@users.noreply.github.com>
2021-05-06 17:46:25 +0300

    Изменение функции подготовки файла
    Отредактирована функция подготовки файлов для обучения. Добавлен вспомогательный массив pointStart в котором хранятся все точки до точки невозврата. В дальнейшем для обучения используется только 3 точки с остаточным ресурсом 1 (эквивалент 100%). 
    Изменена роль переменной flag. Теперь она служит для определения этапа подготовки файлов. 
    Переписана формула для получения ответов после точки невозврата

--- ediag.py
+++ ediag.py
@@ -11,7 +11,7 @@ class EDiag:
         self.name = p
         #load db data
         #load Vector from DB
-
+    #Метод загрузки данных. На вход принимает self - ссылку на самого себя, р - путь до файла
     def loaddata(self, p):
         self.path = p
         if os.path.isdir(p):
@@ -20,13 +20,14 @@ class EDiag:
             self.files = [p]
         else:
             print("It is a special file (socket, FIFO, device file)" )
-
+    #Метод подготовки данных для обчуния нейронной сети. В качестве параметров: self - путь для файлов с показаниями
     def preparation(self):
-        NTRUE = 0
+        NTRUE = 0 # Номер последнего файла до точки невозврата
         N = len(self.files)
-        flag = False
-        tdata = []
-        tout = []
+        flag = 0 # Флаг для определения этапа обучения нейронной сети
+        tdata = [] # Массив вопросов для обучения нейронной сети
+        tout = [] # Массив ответов для нейронной сети
+        startPoint = [] # Массив для значений до точки невозврата
         for i, file in enumerate(self.files):
             FILE = np.genfromtxt(f'{self.path}/{file}', delimiter = '\t')
             vect_pr = self.markup(FILE)
@@ -34,17 +35,32 @@ class EDiag:
             x = np.var(FILE[:, 0])
             print(f'loaded file {i+1}/{N}', end = '\r')
                    #KURT
-            if abs(vect_pr[0] - nu) > 2 * x and not flag:
-                flag = True
-            if flag:
+            # Определяем точку невозврата. Если точка найдена flag = 1
+            if abs(vect_pr[0] - nu) > 2 * x and flag == 0:
+                flag = 1
+                
+            # Пока точка не найдена, записываем во вспомогательный массив все значения
+            if flag == 0:
+                startPoint.append(vect_pr / np.linalg.norm(vect_pr))
+            
+            # Если точка невозврата найдена, записываем в основной массив только три значения - вопроса, для которых ответ будет 1 - эквивален 100% остаточного ресурса.
+            # Изменяем значение flag = 2, последующие значения будем сразу записывать в основной массив вопросов
+            if flag == 1:
+                NTRUE = len(startPoint) - 1
+                tdata.append(startPoint[0])
+                tdata.append(startPoint[-1])
+                tdata.appent(startPoint[round(NTRUE / 2]))
+                flag = 2
+                for i in 3:
+                    tout.append(1)
+                    
+            # Записываем вопросы и ответы после точки невозврата
+            if flag == 2:
                 tdata.append(vect_pr / np.linalg.norm(vect_pr))
-                tout.append(1 - (i - NTRUE) / N)
-            else:
-                NTRUE += 1
-                tdata.append(vect_pr / np.linalg.norm(vect_pr))
-                tout.append(1)
+                tout.append(tout[-1] - (1 / (N - TRUE - 1))
+
         print(f'Loaded all {N} files, NTRUE = {NTRUE}')
-        #тут я тип обучаю
+        # Обучение
         n = NN()
         #print(np.array(tout))
         #print(np.array(tdata))
@@ -53,6 +69,7 @@ class EDiag:
         n.save('model')
         #return (np.array(tdata), np.array(tout))
 
+    #Метод вычисления признаков для нейронной сети. В качестве параметров: FILE - файл с текущими показаниями, self - ссылка на самого себя. Возвращает вектор признаков.
     def markup(self, FILE):
         #FILE = np.genfromtxt(f'{self.path}/{file}', delimiter = '\t')
         n = len(FILE[:, 0])
@@ -63,9 +80,9 @@ class EDiag:
         SF = RMS / ((sum(np.absolute(FILE[:, 0]))) / n)
         PvT = max(np.absolute(FILE[:, 0]))
         CF = PvT / RMS
-        v = np.array([KURT, VARIANCE, RMS, SF, PvT, CF])
-        #return v / np.linalg.norm(v)
-        return v
+        vect = np.array([KURT, VARIANCE, RMS, SF, PvT, CF])
+        #return vect / np.linalg.norm(v)
+        return vect
 
     def markupfrompathnorm(self, file):
         FILE = np.genfromtxt(f'{self.path}/{file}', delimiter = '\t')
@@ -77,10 +94,11 @@ class EDiag:
         SF = RMS / ((sum(np.absolute(FILE[:, 0]))) / n)
         PvT = max(np.absolute(FILE[:, 0]))
         CF = PvT / RMS
-        v = np.array([KURT, VARIANCE, RMS, SF, PvT, CF])
-        return v / np.linalg.norm(v)
-        #return v
+        vect = np.array([KURT, VARIANCE, RMS, SF, PvT, CF])
+        return vect / np.linalg.norm(vect)
+        #return vect
 
+    #Метод определения остаточного ресурса. На вход получает self - ссылку на самого себя, vect - вектор признаков. Возвращает численное значение остаточного ресурса
     def remainingresource(self, p):
         n = NN('model')
         logs = []
