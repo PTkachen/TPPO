e47d58e88e83041e6d4d50df6afbb564b0d46fed
teadr1nker <arsmir999@gmail.com>
2021-05-07 19:26:24 +0300

    Тесты обучения

---
+++ learningtest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from ediag import EDiag
+import sys
+import numpy as np
+from nn import NN
+import matplotlib.pyplot as plt
+
+app = EDiag('bearing1')
+app.loaddata('/media/teadr1nker/94AC9FC8AC9FA2F2/dataset/2nd_test')
+data, out = app.preparation()
+n = NN('model')
+got = n.predict(data)
+for i in range(len(got)):
+    print(f'Got: {got[i]}, expected: {out[i]}')
+
+plt.plot(got)
+plt.plot(out)
+plt.legend(['got','expected'])
+plt.savefig('got-expected.png')
