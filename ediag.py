3820b891a5278d21909212b9f0491c9711e3f101
teadr1nker <arsmir999@gmail.com>
2021-05-10 00:49:48 +0300

    Поменял предсказывание

--- ediag.py
+++ ediag.py
@@ -44,6 +44,7 @@ class EDiag:
             # Пока точка не найдена, записываем во вспомогательный массив все значения
             if flag == 0:
                 startPoint.append(vect_pr / np.linalg.norm(vect_pr))
+
             if abs(vect_pr[0] - nu) > 2 * x and flag == 0:
                 flag = 1
 
@@ -72,7 +73,8 @@ class EDiag:
         print('Обучение...')
         n.learn(np.array(tdata), np.array(tout))
         n.save('model')
-        return (np.array(tdata), np.array(tout))
+        print(startPoint)
+        #return (np.array(tdata), np.array(tout))
 
     #Метод вычисления признаков для нейронной сети. В качестве параметров: FILE - файл с текущими показаниями, self - ссылка на самого себя. Возвращает вектор признаков.
     def markup(self, FILE):
@@ -104,7 +106,7 @@ class EDiag:
         #return vect
 
     #Метод определения остаточного ресурса. На вход получает self - ссылку на самого себя, vect - вектор признаков. Возвращает численное значение остаточного ресурса
-    def remainingresource(self, p):
+    def oldremainingresource(self, p):
         n = NN('model')
         rur = []
         for i, log in enumerate(self.files):
@@ -114,3 +116,47 @@ class EDiag:
         return rur
 
         #return n.predict([list(vect)])
+
+    def remainingresource(self, p):
+        NTRUE = 0 # Номер последнего файла до точки невозврата
+        N = len(self.files)
+        flag = 0 # Флаг для определения этапа обучения нейронной сети
+        tdata = [] # Массив вопросов для обучения нейронной сети
+        tout = [] # Массив ответов для нейронной сети
+        startPoint = [] # Массив для значений до точки невозврата
+        for i, file in enumerate(self.files):
+            FILE = np.genfromtxt(file, delimiter = '\t')
+            vect_pr = self.markup(FILE)
+            nu = np.average(FILE[:, 0])
+            x = np.var(FILE[:, 0])
+            print(f'Загружен файл {i+1}/{N}', end = '\r')
+                   #KURT
+
+            # Определяем точку невозврата. Если точка найдена flag = 1
+            # Пока точка не найдена, записываем во вспомогательный массив все значения
+            if flag == 0:
+                startPoint.append(vect_pr / np.linalg.norm(vect_pr))
+
+            if abs(vect_pr[0] - nu) > 2 * x and flag == 0:
+                flag = 1
+
+
+            # Если точка невозврата найдена, записываем в основной массив только три значения - вопроса, для которых ответ будет 1 - эквивален 100% остаточного ресурса.
+            # Изменяем значение flag = 2, последующие значения будем сразу записывать в основной массив вопросов
+            if flag == 1:
+                NTRUE = len(startPoint) - 1
+                tdata.append(startPoint[0])
+                tdata.append(startPoint[-1])
+                tdata.append(startPoint[round(NTRUE / 2)])
+                flag = 2
+                for i in range(3):
+                    tout.append(1)
+
+            # Записываем вопросы и ответы после точки невозврата
+            if flag == 2:
+                tdata.append(vect_pr / np.linalg.norm(vect_pr))
+                tout.append(tout[-1] - (1 / (N - NTRUE - 1)))
+
+        print('Предсказание')
+        n = NN('model')
+        return n.predict(np.array(tdata))
