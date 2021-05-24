1a11b9627d138bd48f005588c56650e612bbdbf6
PTkachen <PTkachen@gmail.com>
2021-05-24 19:45:42 +0300

    create_setup

--- create_setup.sh
+++ create_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt install python3-pip debmake debhelper-compat dh-python python3-all mysql-server
+sudo pip3 install setuptools numpy matplotlib tensorflow scipy mysql-connector-python
+
 python3 setup.py sdist
 
 cd dist && mv equipmentdiagnostics*.tar.gz ../
