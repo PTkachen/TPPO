f7579ead1b9c948c1717e91ad69752095e092dba
PTkachen <83370779+PTkachen@users.noreply.github.com>
2021-05-27 15:43:58 +0300

    Fixed create_setup.sh
    Fixed create_setup.sh. Added detect_delimiter to python installation packages

---
+++ create_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt install python3-pip debmake debhelper-compat dh-python python3-all mysql-server
+sudo pip3 install setuptools numpy matplotlib tensorflow scipy mysql-connector-python detect_delimiter
+
+python3 setup.py sdist
+
+cd dist && mv equipmentdiagnostics*.tar.gz ../
+cd ..
+
+#mv equipmentdiagnostics*.tar.gz deb
+
+mkdir deb
+tar -C "deb" -xzmf equipmentdiagnostics*.tar.gz
+cd deb
+
+cd equipmentdiagnostics*
+
+sudo debmake -n -b':python3'
+
+sudo debuild
