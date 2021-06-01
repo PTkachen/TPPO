2f07c3cdc85eca6fc6cb7f643c260656f9946439
Juhani <70961963+teadr1nker@users.noreply.github.com>
2021-06-01 10:21:01 +0300

    comments

--- equipmentdiagnostics/nn/nn.py
+++ equipmentdiagnostics/nn/nn.py
@@ -7,12 +7,12 @@ from tensorflow import keras
 
 class NN:
     model = Sequential()
-
+    
     def __init__(self, path=''):
         if path:
             self.model = keras.models.load_model(path)
             print(f'NN debug: loaded model {path}')
-
+    #Метод обучения модели нейронной сети
     def learn(self, x, y):
         # x: np.array(n, 6), y: np.array(n)
         if len(x) != len(y):
@@ -33,10 +33,11 @@ class NN:
         self.model.compile(loss='binary_crossentropy',
                            optimizer='adam', metrics=['accuracy'])
         self.model.fit(x, y, epochs=150, batch_size=10, verbose=0)
-
+    
+    #Метод предсказывания по характеристикам
     def predict(self, X):
         predictions = self.model.predict(X)
         return predictions
-
+    #Метод сохранения модели
     def save(self, path):
         self.model.save(path)
